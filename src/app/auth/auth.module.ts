e79f983c7b46909b026d66e9f2b26b2fdb90ee92
abdulazizladan <abdulazizladan@gmail.com>
2021-02-24 15:26:45 +0100

    style: Set auth background

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 //MaterialModule
 import { MatCardModule } from '@angular/material/card';
